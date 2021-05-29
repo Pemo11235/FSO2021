63377504ce101d8670a10dbb9d4ba606dcfabe22
Pemo11235 <matteoluceri@msn.com>
2021-05-29 23:22:12 +0200

    added 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,26 +8,15 @@ const Button = ({handleClick, text}) => {
     return <button title={text} onClick={handleClick}>{text} </button>
 }
 
-const Display = ({good, neutral, bad}) => {
-    const lines = {
-        good: {
-            text: 'Good',
-            value: good,
-        },
-        neutral: {
-            text: 'Neutral',
-            value: neutral,
-        },
-        bad: {
-            text: 'Bad',
-            value: bad,
-        }
-    }
+const Display = ({lines}) => {
     return (
         <>
             <Line elem={lines.good}/>
             <Line elem={lines.neutral}/>
             <Line elem={lines.bad}/>
+            <Line elem={lines.all}/>
+            <Line elem={lines.average}/>
+            <Line elem={lines.positive}/>
         </>
     )
 }
@@ -41,10 +30,49 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0);
+    const [average, setAverage] = useState(0);
+    const lines = {
+        good: {
+            text: 'Good',
+            value: good,
+        },
+        neutral: {
+            text: 'Neutral',
+            value: neutral,
+        },
+        bad: {
+            text: 'Bad',
+            value: bad,
+        },
+        all: {
+            text: 'All',
+            value: all,
+        },
+        average: {
+            text: 'Average',
+            value: all === 0 ? 0 : average / all,
+        },
+        positive: {
+            text: 'Positive',
+            value: all === 0 ? 0 : (good * 100) / all + ' %',
+        }
+    }
 
-    const handleGood = () => setGood(good + 1)
-    const handleNeutral = () => setNeutral(neutral + 1)
-    const handleBad = () => setBad(bad + 1)
+    const handleGood = () => {
+        setGood(good + 1)
+        setAll(all + 1)
+        setAverage(average + 1)
+    }
+    const handleNeutral = () => {
+        setNeutral(neutral + 1)
+        setAll(all + 1)
+    }
+    const handleBad = () => {
+        setBad(bad + 1)
+        setAll(all + 1)
+        setAverage(average - 1)
+    }
 
     return (
         <div>
@@ -52,8 +80,8 @@ const App = () => {
             <Button handleClick={handleGood} text={'Good'}/>
             <Button handleClick={handleNeutral} text={'Neutral'}/>
             <Button handleClick={handleBad} text={'Bad'}/>
-            <Header text={'Statistics'} />
-            <Display good={good} neutral={neutral} bad={bad}/>
+            <Header text={'Statistics'}/>
+            <Display lines={lines}/>
         </div>
     )
 }
