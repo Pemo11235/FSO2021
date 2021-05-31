ad78d2448765dd5abb1787e949e86be31f9bf8bf
Pemo11235 <matteoluceri@msn.com>
2021-05-31 23:13:24 +0200

    Added 2.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -17,35 +17,42 @@ const Content = ({parts}) => {
     )
 }
 
+const Course = ({course}) => {
+    return(
+        <div>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            {/*<Total exercises={course.parts.map(part => part.exercises)}/>*/}
+        </div>
+    )
+}
 
 const Total = ({exercises}) => {
     return <p>Number of exercises {exercises[0] + exercises[1] + exercises[2]}</p>
 }
 const App = () => {
     const course = {
+        id: 1,
         name: 'Half Stack application development',
         parts: [
             {
                 name: 'Fundamentals of React',
-                exercises: 10
+                exercises: 10,
+                id: 1
             },
             {
                 name: 'Using props to pass data',
-                exercises: 7
+                exercises: 7,
+                id: 2
             },
             {
                 name: 'State of a component',
-                exercises: 14
+                exercises: 14,
+                id: 3
             }
         ]
     }
-    return (
-        <div>
-            <Header course={course.name}/>
-            <Content parts={course.parts}/>
-            <Total exercises={course.parts.map(part => part.exercises)}/>
-        </div>
-    )
+    return <Course course={course} />
 }
 
 export default App
