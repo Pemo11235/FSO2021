0bf16b2cfcc690e99e3118fbb2ce52554b0c7def
Pemo11235 <matteoluceri@msn.com>
2021-05-31 23:30:43 +0200

    Added 2.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,35 +1,6 @@
 import React from 'react'
+import Course from "./Course";
 
-const Header = ({course}) => {
-    return (
-        <h1>{course}</h1>
-    )
-}
-const Part = ({part, exercise}) => {
-    return <p>{part} {exercise}</p>
-}
-
-const Content = ({parts}) => {
-    return (
-        <div>
-            {parts.map(part => <Part key={part.name} part={part.name} exercise={part.exercises}/>)}
-        </div>
-    )
-}
-
-const Course = ({course}) => {
-    return (
-        <div>
-            <Header course={course.name}/>
-            <Content parts={course.parts}/>
-            <Total exercises={course.parts.map(part => part.exercises)}/>
-        </div>
-    )
-}
-
-const Total = ({exercises}) => {
-    return <p><b>Number of exercises {exercises.reduce((acc, curr) => acc += curr)}</b></p>
-}
 const App = () => {
     const courses = [
         {
