a637dc585a2d16ab32aa3385cc1f62823df78915
pemo11235 <matteoluceri@msn.com>
2021-07-04 23:29:07 +0200

    Added 4.5

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -11,7 +11,27 @@ const totalLikes = (blogs) => {
 	}
 	return 0;
 }
+
+const  favoriteBlog = (blogs) => {
+	if(blogs.length < 1){
+		return {
+			title: '',
+			author: '',
+			likes: 0,
+		}
+	}
+	const likes = blogs.map((blog) => blog.likes)
+	const max = Math.max(...likes)
+	const favoriteBlog = blogs.find(blog => blog.likes === max)
+
+	return {
+		title: favoriteBlog.title,
+		author: favoriteBlog.author,
+		likes: favoriteBlog.likes,
+	}
+}
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
