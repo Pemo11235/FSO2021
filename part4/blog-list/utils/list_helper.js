be5f83f2639048de4af994255b294a4108188359
pemo11235 <matteoluceri@msn.com>
2021-07-04 22:59:12 +0200

    Added 4.4

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
 	return 1;
 }
 
+const totalLikes = (blogs) => {
+	if(blogs.length > 0) {
+		const reducer = (totLikes, blogLike) => {
+			return totLikes + blogLike
+		}
+		return blogs.map((blog) => blog.likes).reduce(reducer, 0)
+	}
+	return 0;
+}
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
