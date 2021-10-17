57601d2ecf4324d981e30c53c533e1782eecc977
pemo11235 <matteoluceri@msn.com>
2021-10-17 18:02:03 +0200

    feat(part4): 4.20
    set tokenExtractor as middleware

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -25,9 +25,18 @@ const errorHandler = (error, request, response, next) => {
   }
   next(error);
 };
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization') 
+    if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        // subtring take from [7 char, end of string]
+        request.token = authorization.substring(7)
+    }
+    next()
+}
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor
 };
