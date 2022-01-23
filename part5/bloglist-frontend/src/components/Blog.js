4dd651bdb863e02ef7ffd6102f135314d9517470
Pemo11235 <matteoluceri@msn.com>
2022-01-23 22:48:45 +0100

    feat(part5): 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,29 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({ blog }) => {
+  const [details, setDetails] = React.useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} - {blog.author}
+        <button onClick={() => setDetails(!details)}>view</button>
+        {details && (
+          <div>
+            URL: {blog.url} <br />
+            Likes: {blog.likes} <button>Like</button>
+            <br />
+            User: {blog.user.username}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
