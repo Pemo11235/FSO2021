6219043567dfff24a62dc294ac38025e4c2f8742
Pemo11235 <matteoluceri@msn.com>
2022-05-10 11:19:01 +0200

    feat(part7): add 7.18

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -11,61 +11,83 @@ blogsRouter.get('/', async (request, response) => {
 
 });
 
-blogsRouter.post('/', userExtractor, async (request, response) => {
-    const body = request.body;
+blogsRouter.post("/", userExtractor, async (request, response) => {
+  const body = request.body;
 
-    if(!request.token) {
-        return response.status(401).json({error: 'token missing or invalid'})
-    }
+  if (!request.token) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
 
-    const user = request.user 
+  const user = request.user;
 
-    if ((body.title === undefined || null) && (body.url === undefined || null)) {
-        response.status(400).end()
-    } else {
+  if ((body.title === undefined || null) && (body.url === undefined || null)) {
+    response.status(400).end();
+  } else {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes ?? 0,
+      user: user._id,
+      comments: body.comments ?? [],
+    });
 
-        const blog = new Blog({
-            title: body.title,
-            author: body.author,
-            url: body.url,
-            likes: body.likes ?? 0,
-            user: user._id
-        })
+    const result = await blog.save();
 
-        const result = await blog.save()
+    user.blogs = user.blogs.concat(result._id);
+    await user.save();
 
-        user.blogs = user.blogs.concat(result._id)
-        await user.save()
-
-        response.status(201).json(result)
-    }
-
+    response.status(201).json(result);
+  }
 });
 
+blogsRouter.delete("/:id", userExtractor, async (request, response) => {
+  let idToDelete = request.params.id;
 
-blogsRouter.delete('/:id', userExtractor , async (request, response) => {
-    let idToDelete = request.params.id;
-    
-    const blogToCheckBeforeRemove = await Blog.findById(idToDelete)
-    const user = request.user 
-    
-    if(blogToCheckBeforeRemove.user.toString() === user._id.toString()) {
-        const result = await Blog.findByIdAndRemove(idToDelete);
-        response.status(204).end();
-        return;
-    }
-    
-    response.status(401).json({error: "Unauthorizated user for this operation!"}).end();
+  const blogToCheckBeforeRemove = await Blog.findById(idToDelete);
+  const user = request.user;
+
+  if (blogToCheckBeforeRemove.user.toString() === user._id.toString()) {
+    const result = await Blog.findByIdAndRemove(idToDelete);
+    response.status(204).end();
     return;
+  }
+
+  response
+    .status(401)
+    .json({ error: "Unauthorizated user for this operation!" })
+    .end();
+  return;
 });
 
-blogsRouter.put('/:id', async (request,response) => {
-    let blogToUpdate = request.body;
-    const blogObj = new Blog(blogToUpdate).toJSON();
-    delete blogObj.id;
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id , blogObj, {new: true});
-    response.json(updatedBlog);
+blogsRouter.put("/:id", async (request, response) => {
+  let blogToUpdate = request.body;
+  const blogObj = new Blog(blogToUpdate).toJSON();
+  delete blogObj.id;
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blogObj, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
 
+blogsRouter.put("/:id/comments", async (request, response) => {
+  const commentsToPut = request.body;
+  const blogID = request.params.id;
+
+  const blogToUpdate = await Blog.findById(blogID);
+
+  const blogObj = { comments: commentsToPut.comments, ...blogToUpdate };
+  const blogJSON = new Blog(blogObj).toJSON();
+  delete blogJSON.id;
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    blogJSON,
+    {
+      new: true,
+    }
+  );
+  response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
