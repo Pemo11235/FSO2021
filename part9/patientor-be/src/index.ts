5d808c9f3f38e7d2ef892d3f50433f8bd4442192
Pemo11235 <matteoluceri@msn.com>
2022-05-27 12:31:05 +0200

    feat(part9): add styles formatter

--- part9/patientor-be/src/index.ts
+++ part9/patientor-be/src/index.ts
@@ -1,3 +1,15 @@
-import { express } from 'express'
+import express from "express";
 
-const
+const app = express();
+app.use(express.json());
+
+const PORT = 3003;
+
+app.get("/ping", (_req, res) => {
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server started at http://localhost:${PORT}`);
+});
