e91cef8d7ab1f525e3efc98e0048fe8f56bae84d
Pemo11235 <matteoluceri@msn.com>
2022-06-03 18:05:41 +0200

    feat(part9): removed submodule

---
+++ part9/patientor/src/PatientPage.tsx
@@ -0,0 +1,8 @@
+import { TableRow, TableCell } from '@material-ui/core'
+import HealthRatingBar from './components/HealthRatingBar'
+import { Patient } from './types'
+const PatientPage = ({ patient }: { patient: Patient }) => {
+  return <></>
+}
+
+export default PatientPage
